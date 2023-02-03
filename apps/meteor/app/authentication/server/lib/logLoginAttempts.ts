d4a0b7efe22ced41ff69cce294e7c5222df8953f
Wojciech Barcik <wbarcik@syncad.com>
2023-02-03 16:35:38 +0100

    Enhance logging of login attempts

--- apps/meteor/app/authentication/server/lib/logLoginAttempts.ts
+++ apps/meteor/app/authentication/server/lib/logLoginAttempts.ts
@@ -8,8 +8,8 @@ export const logFailedLoginAttempts = (login: ILoginAttempt): void => {
 	}
 
 	let user = 'unknown';
-	if (login.methodArguments[0]?.user?.username && settings.get('Login_Logs_Username')) {
-		user = login.methodArguments[0]?.user?.username;
+	if (settings.get('Login_Logs_Username')) {
+		user = login.methodArguments[0]?.user?.username || login.user?.username || 'unknown';
 	}
 	const { connection } = login;
 	let { clientAddress } = connection;
@@ -26,7 +26,49 @@ export const logFailedLoginAttempts = (login: ILoginAttempt): void => {
 	if (!settings.get('Login_Logs_UserAgent')) {
 		userAgent = '-';
 	}
-	SystemLogger.info(
-		`Failed login detected - Username[${user}] ClientAddress[${clientAddress}] ForwardedFor[${forwardedFor}] XRealIp[${realIp}] UserAgent[${userAgent}]`,
+	const type = login.type || 'unknown';
+	SystemLogger.warn(
+		`Failed login detected - Type[${type}] Username[${user}] ClientAddress[${clientAddress}] ForwardedFor[${forwardedFor}] XRealIp[${realIp}] UserAgent[${userAgent}]`,
+	);
+};
+
+export const logLoginAttempts = (login: ILoginAttempt): void => {
+	if (!settings.get('Login_Logs_Enabled')) {
+		return;
+	}
+
+	if (login.type === 'resume') {
+		//
+		// `resume` occurs when session is regenerated, e.g. when user
+		// reloads the page. We don't want to log on this event.
+		//
+		// TODO Unfortunately this also occurs when user logs in via
+		// iframe.
+		//
+		return;
+	}
+
+	let user = 'unknown';
+	if (settings.get('Login_Logs_Username')) {
+		user = login.methodArguments[0]?.user?.username || login.user?.username || 'unknown';
+	}
+	const { connection } = login;
+	let { clientAddress } = connection;
+	if (!settings.get('Login_Logs_ClientIp')) {
+		clientAddress = '-';
+	}
+	let forwardedFor = connection.httpHeaders?.['x-forwarded-for'];
+	let realIp = connection.httpHeaders?.['x-real-ip'];
+	if (!settings.get('Login_Logs_ForwardedForIp')) {
+		forwardedFor = '-';
+		realIp = '-';
+	}
+	let userAgent = connection.httpHeaders?.['user-agent'];
+	if (!settings.get('Login_Logs_UserAgent')) {
+		userAgent = '-';
+	}
+	const type = login.type || 'unknown';
+	SystemLogger.warn(
+		`Login attempt detected - Type[${type}] Username[${user}] ClientAddress[${clientAddress}] ForwardedFor[${forwardedFor}] XRealIp[${realIp}] UserAgent[${userAgent}]`,
 	);
 };
