c03bfd9a1dbdf1da62bd0614cc779ce399d6a2ac
Wojciech Barcik <wbarcik@syncad.com>
2023-03-08 16:46:31 +0100

    Add basic storage listener

--- apps/meteor/client/views/root/AppRoot.tsx
+++ apps/meteor/client/views/root/AppRoot.tsx
@@ -11,18 +11,37 @@ const AppLayout = lazy(() => import('./AppLayout'));
 const PortalsWrapper = lazy(() => import('./PortalsWrapper'));
 const ModalRegion = lazy(() => import('../modal/ModalRegion'));
 
-const AppRoot: FC = () => (
-	<Suspense fallback={<PageLoading />}>
-		<QueryClientProvider client={queryClient}>
-			<MeteorProvider>
-				<ConnectionStatusBar />
-				<BannerRegion />
-				<AppLayout />
-				<PortalsWrapper />
-				<ModalRegion />
-			</MeteorProvider>
-		</QueryClientProvider>
-	</Suspense>
-);
+const AppRoot: FC = () => {
+	window.addEventListener('storage', (e) => {
+		console.log('storageEvent', e);
+		const redirectToPathname = '/home';
+		if (e.key === 'Meteor.userId') {
+			if (e.newValue) {
+				console.log('storageEvent login');
+			} else {
+				console.log('storageEvent logout');
+				// TODO Post message to parent window, when we're in iframe.
+			}
+			console.log('storageEvent redirecting to', redirectToPathname);
+
+			window.location.assign(redirectToPathname);
+			// TODO it would be better to redirect using router.
+		}
+	});
+
+	return (
+		<Suspense fallback={<PageLoading />}>
+			<QueryClientProvider client={queryClient}>
+				<MeteorProvider>
+					<ConnectionStatusBar />
+					<BannerRegion />
+					<AppLayout />
+					<PortalsWrapper />
+					<ModalRegion />
+				</MeteorProvider>
+			</QueryClientProvider>
+		</Suspense>
+	);
+};
 
 export default AppRoot;
