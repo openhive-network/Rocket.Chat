769f9391224184677bcb63861763afa766c94be4
wojtek555 <wbarcik@syncad.com>
2023-03-15 18:08:27 +0100

    [NEW] Handle logout in other tab (#4)
    * Add basic storage listener
    
    * Update docstrings and TODO
    
    * Remove console logs

--- apps/meteor/client/views/root/AppRoot.tsx
+++ apps/meteor/client/views/root/AppRoot.tsx
@@ -9,16 +9,30 @@ import PageLoading from './PageLoading';
 const MeteorProvider = lazy(() => import('../../providers/MeteorProvider'));
 const AppLayout = lazy(() => import('./AppLayout'));
 
-const AppRoot = (): ReactElement => (
-	<OutermostErrorBoundary>
-		<Suspense fallback={<PageLoading />}>
-			<QueryClientProvider client={queryClient}>
-				<MeteorProvider>
-					<AppLayout />
-				</MeteorProvider>
-			</QueryClientProvider>
-		</Suspense>
-	</OutermostErrorBoundary>
-);
+
+const AppRoot = (): ReactElement => {
+	window.addEventListener('storage', (e) => {
+		const redirectToPathname = '/home';
+		if (e.key === 'Meteor.userId') {
+			if (e.newValue) {
+				console.log('storageEvent login');
+				// TODO When user changes we're in trouble – user sees
+				// doubled channels in Channels list in menu.
+			}
+			window.location.assign(redirectToPathname);
+		}
+	});
+	return (
+		<OutermostErrorBoundary>
+			<Suspense fallback={<PageLoading />}>
+				<QueryClientProvider client={queryClient}>
+					<MeteorProvider>
+						<AppLayout />
+					</MeteorProvider>
+				</QueryClientProvider>
+			</Suspense>
+		</OutermostErrorBoundary>
+	)
+};
 
 export default AppRoot;
